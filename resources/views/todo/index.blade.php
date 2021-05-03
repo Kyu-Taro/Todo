009bf4012f125ac714d6fe72a98a14435ebbaf37
KyuKyu <gtnine999@gmail.com>
2021-05-04 00:25:10 +0900

    css修正

--- resources/views/todo/index.blade.php
+++ resources/views/todo/index.blade.php
@@ -1,19 +1,21 @@
 @extends('layouts.app')
 
 @section('content')
-<form>
-    <div class="mb-3">
-        <input type="text" id="disabledTextInput" class="form-control">
+<div class="todo-container">
+    <form class="todo-add-form" method="POST" action="">
+        <input type="text" id="disabledTextInput" class="form-input">
+        <button type="submit" class="btn btn-primary">追加</button>
+    </form>
+    @foreach ($data as $value)
+    <div class="card">
+        <div class="card-body">
+            {{ $value->content }}
+            <div class="fix-btn">
+                <button type="button" class="btn btn-success">編集</button>
+                <button type="button" class="btn btn-danger">削除</button>
+            </div>
+        </div>
     </div>
-    <button type="submit" class="btn btn-primary">追加</button>
-</form>
-@foreach ($data as $value)
-<div class="card">
-    <div class="card-body">
-        {{ $value->content }}
-        <button type="button" class="btn btn-success">編集</button>
-        <button type="button" class="btn btn-danger">削除</button>
-    </div>
+    @endforeach
 </div>
-@endforeach
 @endsection
