1e2dc446edf4793a56b2dbcd5f9b4685ee4fe231
KyuKyu <gtnine999@gmail.com>
2021-05-03 23:50:51 +0900

    投稿の一覧表示と入力フォーム、削除ボタンの配置

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Todo;
+use Illuminate\Support\Facades\Auth;
 
 class TodoController extends Controller
 {
     public function index()
     {
-        return view('todo.index');
+        $data = Todo::where('user_id', Auth::id())->get();
+        return view('todo.index', compact('data'));
     }
 }
--- resources/views/todo/index.blade.php
+++ resources/views/todo/index.blade.php
@@ -1,5 +1,18 @@
 @extends('layouts.app')
 
 @section('content')
-<h1>こんにちは</h1>
+<form>
+    <div class="mb-3">
+        <input type="text" id="disabledTextInput" class="form-control">
+    </div>
+    <button type="submit" class="btn btn-primary">追加</button>
+</form>
+@foreach ($data as $value)
+<div class="card">
+    <div class="card-body">
+        {{ $value->content }}
+        <button type="button" class="btn btn-danger">削除</button>
+    </div>
+</div>
+@endforeach
 @endsection
