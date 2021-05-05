34fc0e3493e1c8591a6e1f2b972c80d7f0e9b877
KyuKyu <gtnine999@gmail.com>
2021-05-05 22:17:39 +0900

    ジョブとキューでログインデーターをSotrageでファイルに書き出し

---
+++ app/Jobs/UserLoginJob.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Storage;
+use App\User;
+
+class UserLoginJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public $user;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct(User $user)
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        Storage::append('userLogin.txt', 'ユーザー'. $this->user->name . 'が' . $this->user->updated_at . 'にログインしました');
+    }
+}
